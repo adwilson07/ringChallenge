c4c03c4acc321ad1b14158d286973b8f25bec79e
austinw <austinw@gogoair.com>
2020-03-18 18:32:26 -0600

    Delete output folder

--- out/production/resources/db/sql/create-db.sql
+++
@@ -1,7 +0,0 @@
---DROP TABLE Viruses IF EXISTS;
-
-CREATE TABLE Viruses (
-  id         INTEGER PRIMARY KEY,
-  name VARCHAR(30),
-  email  VARCHAR(50)
-);
--- out/production/resources/db/sql/insert-data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO Viruses VALUES (1, 'ebola', 'epidemic');
-INSERT INTO Viruses VALUES (2, 'corona', 'pandemic');
-INSERT INTO Viruses VALUES (3, 'H1N1', 'quarantine');
-INSERT INTO Viruses VALUES (4, 'flu', 'noBodyCares');
